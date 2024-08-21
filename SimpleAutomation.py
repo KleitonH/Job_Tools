817a2eee676b04f0932b82a337eaf42c4eaaff1f
KleitonH <kleitonhomemmartins@rede.ulbra.br>
2024-08-21 14:16:13 -0300

    small fix

--- SimpleAutomation.py
+++ SimpleAutomation.py
@@ -1,12 +1,14 @@
 #Algoritmo de atribuição de código de seção fixo
 import pyautogui # Importação da biblioteca Pyautogui para controle do mouse e teclado 
-import time # Importação da biblioteca time para intervalos de tempo 
+import time # Importação da biblioteca time para intervalos de tempo
+import Breaker
 from pytesseract import pytesseract # Importação da biblioteca pytesseract para conversão de imagens em strings
 caminho_tesseract = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe" # Caminho para o executável do pytesseract
 pytesseract.tesseract_cmd = caminho_tesseract # Define o caminho para o pytesseract
 tempo_de_execucao = 40000
 tempo_inicial = time.time() # Regra para definir o período de tempo
-while(time.time() - tempo_inicial) < tempo_de_execucao:
+monitor_thread = Breaker.iniciar_monitoramento()
+while(time.time() - tempo_inicial) < tempo_de_execucao and Breaker.verificar_interrupcao():
     time.sleep(1)
     pyautogui.typewrite("5073")
     time.sleep(0.2) 
